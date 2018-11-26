95424742a8b11b5458c87aa10eeb590a7639ffc2
Anna Rae Hazarian <annaraehazarian@gmail.com>
2018-11-25 16:06:26 -0800

    added filter function on groupresults

--- js/lab4.js
+++ js/lab4.js
@@ -14,6 +14,55 @@ var complexData = [
 ]
 
 
+// Thanks w3 schools! https://www.w3schools.com/howto/howto_js_filter_elements.asp
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
   console.log('hello world');
@@ -25,7 +74,6 @@ $(document).ready(function() {
   var parentDiv = $("#templatedProjects");
 
   // BEGIN - STEP 1
-  /*
 
   // start with a simple template
   var html = template(simpleData);
@@ -39,12 +87,12 @@ $(document).ready(function() {
     parentDiv.append(curHtml);
   }
 
-  */
+
   // END - STEP 1
 
 
   // BEGIN - STEP 2
-  /*
+
 
   // when you first load the page, set a custom name if you have one:
   var name = localStorage.getItem('customName');
@@ -61,12 +109,12 @@ $(document).ready(function() {
     }
   });
 
-  */
+
   // END - STEP 2
 
 
   // BEGIN - STEP 3
-  /*
+
 
   // Use the URLSearchParams API to make fake-database queries using a URL
   // https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
@@ -84,6 +132,6 @@ $(document).ready(function() {
     }
   }
 
-  */
+
   // END - STEP 3
 });
